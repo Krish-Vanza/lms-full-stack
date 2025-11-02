ebf5de82573a4db72eaa38fe4835384a17af1e1a
Krish-Vanza <149684377+Krish-Vanza@users.noreply.github.com>
2025-11-03 00:33:32 +0530

    Not free anymore

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -48,7 +48,7 @@ export const purchaseCourse = async (req, res) => {
         const purchaseData = {
             courseId: courseData._id,
             userId,
-            amount: isNitwStudent ? 0 :(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2),
+            amount: isNitwStudent ? 0.01 :(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2),
         }
 
         const newPurchase = await Purchase.create(purchaseData)
